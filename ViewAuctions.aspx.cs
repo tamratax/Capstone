03cc0bb844e3435b5c3f2e43aa0f277ecaaaa795
Eli Raich <raicheb@dukes.jmu.edu>
2021-03-23 16:47:49 -0400

    Search bar to view auction

--- ViewAuctions.aspx.cs
+++ ViewAuctions.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Drawing;
 
 namespace Lab3
 {
@@ -12,6 +16,42 @@ namespace Lab3
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            string sqlQueryDuplicate = "Select Customer.CustomerID, Firstname +' '+ Lastname as Name, TicketOpenDate as OpenDate, EmpFirstName + ' ' + EmpLastName as InitiatingEmployee from ServiceTicket join customer on ServiceTicket.CustomerID = Customer.CustomerID join employee on employee.employeeID = ServiceTicket.InitiatingEmp where ServiceType = 'Auction' AND  Firstname +' '+ Lastname Like @name order by TicketOpenDate DESC";
+
+            SqlConnection sqlConnectDuplicate = new SqlConnection("Server=Localhost;Database=Lab3;Trusted_Connection=Yes;");
+            sqlConnectDuplicate.Open();
+
+            SqlCommand sqlCommandInsert = new SqlCommand();
+            sqlCommandInsert.Connection = sqlConnectDuplicate;
+            sqlCommandInsert.CommandType = CommandType.Text;
+            sqlCommandInsert.CommandText = sqlQueryDuplicate;
+            sqlCommandInsert.Parameters.Add(new SqlParameter("@Name", "%" + HttpUtility.HtmlEncode(TxtSearch.Text) + "%"));
+
+            SqlDataAdapter sqlAdapterDuplicate = new SqlDataAdapter(sqlCommandInsert);
+            //The adapter is the bridge that pulls in both the query and the connection and stores it in adapter
+            //SqlDataAdapter sqlAdapterDuplicate = new SqlDataAdapter(sqlQueryDuplicate, sqlConnectDuplicate); <- This is how we originally did it with a DataAdapter
+
+            //This creates a datatable and fills it
+
+            //DataTable dtForDuplicate = new DataTable();
+            //sqlAdapterDuplicate.Fill(dtForDuplicate);
+
+            //GridviewMoves.DataSource = dtForDuplicate;
+            //GridviewMoves.DataBind();
+            //GridviewMoves.Visible = true;
+
+            DataSet ds = new DataSet();
+
+            sqlAdapterDuplicate.Fill(ds);
+
+            GridviewAuction.DataSource = ds;
+
+            GridviewAuction.DataBind();
+            GridviewAuction.Visible = true;
         }
     }
 }
--- ViewAuctions.aspx.designer.cs
+++ ViewAuctions.aspx.designer.cs
@@ -15,21 +15,39 @@ namespace Lab3
     {
 
         /// <summary>
-        /// GridviewAuctions control.
+        /// SearchCustomerLbl control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridviewAuctions;
+        protected global::System.Web.UI.WebControls.Label SearchCustomerLbl;
 
         /// <summary>
-        /// dtasrcAuctions control.
+        /// TxtSearch control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource dtasrcAuctions;
+        protected global::System.Web.UI.WebControls.TextBox TxtSearch;
+
+        /// <summary>
+        /// BtnSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSearch;
+
+        /// <summary>
+        /// GridviewAuction control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridviewAuction;
     }
 }
