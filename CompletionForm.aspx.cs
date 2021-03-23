5490f81898fae7fc46ef797f22b5c66aaebd12a9
Daniel Mehari <meharida@dukes.jmu.edu>
2021-03-23 17:26:04 -0400

    Added Servey Email script

--- CompletionForm.aspx.cs
+++ CompletionForm.aspx.cs
@@ -8,6 +8,8 @@ using System.Data.SqlClient;
 using System.Web.Configuration;
 using System.Drawing;
 using System.Data;
+using System.Net;
+using System.Net.Mail;
 
 namespace Lab3
 {
@@ -192,6 +194,30 @@ namespace Lab3
             //{
             //    LblStatus.Text = "Database Error!";
             //}
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            var fromAddress = new MailAddress("RoCoConsulting1@gmail.com", "From RoCoConsulting");
+            var toAddress = new MailAddress("meharida@dukes.jmu.edu", "To Name");
+            const string fromPassword = "RoCo703757";
+            const string subject = "Service Order Successfully Completed!";
+            const string body = "Hello, We are glad to have provided you with our services. If you could leave us a review it would be much appretiated. " +
+                "https://docs.google.com/forms/d/e/1FAIpQLSd2BrUcMwVNyyqqJ0vMiztJcCr78cSajYY1ptFLKMw8xRdcUw/viewform?usp=sf_link";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
+            {
+                smtp.Send(message);
+            }
         }
     }
 }
