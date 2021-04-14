0d24bb7142645cf08bd3b09f42fc6c7c621e8d23
clattjm <clatt2jm@dukes.jmu.edu>
2021-04-14 17:42:05 -0400

    appraisel s.o validation

--- AppraisalServiceOrder.aspx.designer.cs
+++ AppraisalServiceOrder.aspx.designer.cs
@@ -204,6 +204,15 @@ namespace Lab3
         protected global::System.Web.UI.WebControls.TextBox TxtAppraisalSize;
 
         /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// div3 control.
         /// </summary>
         /// <remarks>
@@ -220,6 +229,15 @@ namespace Lab3
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TxtInventory;
+
+        /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
 
         /// <summary>
         /// div4 control.
